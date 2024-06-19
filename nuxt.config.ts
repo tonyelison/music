5c93bc1a47303eeb37e823f0125925fe1def08a4
Anthony Elison <tonyelison37@gmail.com>
2024-06-19 16:00:01 -0600

    Add gh-pages, deploy script, home and 404 pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    baseURL: "/website/",
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,1 @@
+<template>Website</template>
