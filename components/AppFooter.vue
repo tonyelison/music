1b5860b49092072936f07fe57996a771b84197e3
Anthony Elison <tonyelison37@gmail.com>
2024-06-22 01:09:22 -0600

    Move footer styles to component scope, make justification responsive

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,3 +1,21 @@
 <template>
   <footer>© Tony Elison, 2024</footer>
 </template>
+
+<style scoped>
+footer {
+  height: 40px;
+  background-color: black;
+  color: white;
+
+  display: flex;
+  justify-content: right;
+  align-items: center;
+}
+
+@media screen and (max-width: 425px) {
+  footer {
+    justify-content: left;
+  }
+}
+</style>
